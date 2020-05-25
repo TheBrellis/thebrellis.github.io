fe662140698c41b218314caeca2c780b19d47c84
TheBrellis <brianroyalellis@gmail.com>
2020-05-25 18:16:58 -0400

    deploy

--- deploy.sh
+++ deploy.sh
@@ -9,14 +9,11 @@ npm run build
 # navigate into the build output directory
 cd dist
 
-# if you are deploying to a custom domain
-# echo 'www.example.com' > CNAME
-
 git init
 git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io
-git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
+git push -f git@github.com:thebrellis/thebrellis.github.io.git master
 
 cd -
