0915626ee37237da6e32283089392e82a8ee42fb
TheBrellis <brianroyalellis@gmail.com>
2020-05-30 16:14:52 -0400

    set up components and scaffolding for application

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,25 @@
 <template>
   <div id="app">
     <Header />
+    <Bio />
+    <Projects />
+    <Contact />
   </div>
 </template>
 
 <script>
 import Header from "./components/Header.vue";
+import Bio from "./compontents/Bio.vue";
+import Projects from "./components/Projects.vue";
+import Contact from "./components/Contact.vue";
 
 export default {
   name: "App",
   components: {
-    Header
+    Header,
+    Bio,
+    Projects,
+    Contact
   }
 };
 </script>
---
+++ src/components/Projects.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-card elevation="2" class="mx-auto"></v-card>
+</template>
+
+<script>
+export default {
+  name: "Projects",
+  components: {
+    ProjectCard
+  }
+};
+</script>
+
+<style scoped>
+</style>
